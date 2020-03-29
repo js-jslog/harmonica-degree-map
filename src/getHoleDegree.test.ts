833bb4b4bc8119121d696ceea2ce8a113452220e
js-jslog <jhs4jbs@hotmail.co.uk>
2020-03-29 20:00:06 +0000

    Add types to test

--- src/getHoleDegree.test.ts
+++ src/getHoleDegree.test.ts
@@ -1,9 +1,9 @@
 const getHoleDegree = require('./getHoleDegree');
 
 test('Degree at 1blow is 1 when root is at 1blow', () => {
-  const BLOW_1 = 0;
-  const ROOT_AT_BLOW_1 = 0;
-  const MAJOR_1ST_DEGREE = 1;
+  const BLOW_1: 0 = 0;
+  const ROOT_AT_BLOW_1: 0 = 0;
+  const MAJOR_1ST_DEGREE: 1 = 1;
 
   const degree = getHoleDegree(BLOW_1, ROOT_AT_BLOW_1);
 
@@ -11,9 +11,9 @@ test('Degree at 1blow is 1 when root is at 1blow', () => {
 });
 
 test('Degree at 1draw is 2 when root is at 1blow', () => {
-  const DRAW_1 = 2;
-  const ROOT_AT_BLOW_1 = 0;
-  const MAJOR_2ND_DEGREE = 3;
+  const DRAW_1: 2 = 2;
+  const ROOT_AT_BLOW_1: 0 = 0;
+  const MAJOR_2ND_DEGREE: 3 = 3;
 
   const degree = getHoleDegree(DRAW_1, ROOT_AT_BLOW_1);
 
